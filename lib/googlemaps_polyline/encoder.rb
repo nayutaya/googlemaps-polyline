102684d20e98ad9d0e5c0f79062802ca11be70a2
Yuya Kato <yuyakato@gmail.com>
2010-03-20 02:40:49 +0900

    リファクタリング

--- lib/googlemaps_polyline/encoder.rb
+++ lib/googlemaps_polyline/encoder.rb
@@ -4,29 +4,23 @@ module GoogleMapsPolyline
   module Encoder
 
     def self.write_num(io, num)
-p num
-      p negative = (num < 0)
+      negative = (num < 0)
       if negative
         num -= -1
         num *= -1
       end
-p num
 
       num <<= 1
       num  |= 1 if negative
-p num.to_s(2)
 
       codes = []
 
       begin
-p num
         code = (num & 0b11111) + 63
         num >>= 5
-        code |= 0x20 if num > 0 #num <= 0b11111
+        code |= 0x20 if num > 0
         codes << code
-p codes
       end while num > 0
-p codes.map { |x| x.to_s(2) }
 
       return codes.pack("C*")
     end
