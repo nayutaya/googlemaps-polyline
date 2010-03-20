57f1509a291a81ef28728a6264f6050fac1c5f54
Yuya Kato <yuyakato@gmail.com>
2010-03-20 15:28:07 +0900

    リファクタリング

--- test/encoder_test.rb
+++ test/encoder_test.rb
@@ -6,7 +6,6 @@ require "stringio"
 
 class EncoderTest < Test::Unit::TestCase
   def setup
-    @mod = GoogleMapsPolyline::Encoder
     @klass = GoogleMapsPolyline::Encoder
   end
 
@@ -17,37 +16,37 @@ class EncoderTest < Test::Unit::TestCase
   end
 
   def test_write_num
-    assert_equal("?"     , @mod.write_num(sio,         0).string)
-    assert_equal("A"     , @mod.write_num(sio,         1).string)
-    assert_equal("@"     , @mod.write_num(sio,        -1).string)
-    assert_equal("{sopV" , @mod.write_num(sio,  12345678).string)
-    assert_equal("zsopV" , @mod.write_num(sio, -12345678).string)
-    assert_equal("_gsia@", @mod.write_num(sio,  18000000).string)
-    assert_equal("~fsia@", @mod.write_num(sio, -18000000).string)
+    assert_equal("?"     , @klass.write_num(sio,         0).string)
+    assert_equal("A"     , @klass.write_num(sio,         1).string)
+    assert_equal("@"     , @klass.write_num(sio,        -1).string)
+    assert_equal("{sopV" , @klass.write_num(sio,  12345678).string)
+    assert_equal("zsopV" , @klass.write_num(sio, -12345678).string)
+    assert_equal("_gsia@", @klass.write_num(sio,  18000000).string)
+    assert_equal("~fsia@", @klass.write_num(sio, -18000000).string)
   end
 
   def test_encode_points
     assert_equal(
       "",
-      @mod.encode_points(sio, []).string)
+      @klass.encode_points(sio, []).string)
     assert_equal(
       "??",
-      @mod.encode_points(sio, [[0, 0]]).string)
+      @klass.encode_points(sio, [[0, 0]]).string)
     assert_equal(
       "????",
-      @mod.encode_points(sio, [[0, 0], [0, 0]]).string)
+      @klass.encode_points(sio, [[0, 0], [0, 0]]).string)
     assert_equal(
       "A?",
-      @mod.encode_points(sio, [[1, 0]]).string)
+      @klass.encode_points(sio, [[1, 0]]).string)
     assert_equal(
       "?A",
-      @mod.encode_points(sio, [[0, 1]]).string)
+      @klass.encode_points(sio, [[0, 1]]).string)
     assert_equal(
       "AA??",
-      @mod.encode_points(sio, [[1, 1], [1, 1]]).string)
+      @klass.encode_points(sio, [[1, 1], [1, 1]]).string)
     assert_equal(
       "ACACAGAOA_@",
-      @mod.encode_points(sio, [[1, 2], [2, 4], [3, 8], [4, 16], [5, 32]]).string)
+      @klass.encode_points(sio, [[1, 2], [2, 4], [3, 8], [4, 16], [5, 32]]).string)
   end
 
   private
