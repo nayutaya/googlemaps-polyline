da83a3648a29c622f12d42b7eb55c075e66c2728
Yuya Kato <yuyakato@gmail.com>
2010-03-23 14:45:24 +0900

    エンコード処理を追加

--- lib/googlemaps_polyline/core.rb
+++ lib/googlemaps_polyline/core.rb
@@ -1,4 +1,19 @@
 # coding: utf-8
 
+require "googlemaps_polyline/encoder"
+
 module GoogleMapsPolyline
+  def self.encode_polyline_1e5(records)
+    points = []
+    levels = []
+    records.each { |lat, lng, level|
+      points << [lat, lng]
+      levels << level
+    }
+
+    encoded_points = Encoder.new(StringIO.new).encode_points(points).string
+    encoded_levels = Encoder.new(StringIO.new).encode_levels(levels).string
+
+    return encoded_points, encoded_levels
+  end
 end
--- test/core_test.rb
+++ test/core_test.rb
@@ -1,9 +1,23 @@
 # coding: utf-8
 
 require "test_helper"
+require "googlemaps_polyline/core"
 
 class CoreTest < Test::Unit::TestCase
-  def test_true
-    assert(true)
+  def setup
+    @mod = GoogleMapsPolyline
+  end
+
+  def test_encode_polyline_1e5__1
+    points, levels = @mod.encode_polyline_1e5([[0, 0, 3]])
+    assert_equal("??", points)
+    assert_equal("B",  levels)
+  end
+
+  def test_encode_polyline_1e5__1
+    points, levels = @mod.encode_polyline_1e5(
+      [[1, 2, 3], [2, 4, 2], [3, 8, 1], [4, 16, 0], [5, 32, 3]])
+    assert_equal("ACACAGAOA_@", points)
+    assert_equal("BA@?B",       levels)
   end
 end
