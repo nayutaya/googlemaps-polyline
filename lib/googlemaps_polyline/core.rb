ea5625fda03f9c86f1efc3130f327da5088b286c
Yuya Kato <yuyakato@gmail.com>
2010-03-23 15:01:37 +0900

    リファクタリング

--- lib/googlemaps_polyline/core.rb
+++ lib/googlemaps_polyline/core.rb
@@ -4,18 +4,16 @@ require "googlemaps_polyline/encoder"
 require "googlemaps_polyline/decoder"
 
 module GoogleMapsPolyline
-  def self.encode_polyline_1e5(records)
-    points = []
-    levels = []
-    records.each { |lat, lng, level|
-      points << [lat, lng]
-      levels << level
-    }
-
+  def self.encode_points_and_levels(points, levels)
     encoded_points = Encoder.new(StringIO.new).encode_points(points).string
     encoded_levels = Encoder.new(StringIO.new).encode_levels(levels).string
-
     return encoded_points, encoded_levels
+  end
+
+  def self.encode_polyline_1e5(records)
+    points = records.map { |record| record[0..1] }
+    levels = records.map { |record| record[2] }
+    return self.encode_points_and_levels(points, levels)
   end
 
   def self.decode_polyline_1e5(points, levels)
--- test/core_test.rb
+++ test/core_test.rb
@@ -8,6 +8,12 @@ class CoreTest < Test::Unit::TestCase
     @mod = GoogleMapsPolyline
   end
 
+  def test_encode_points_and_levels
+    points, levels = @mod.encode_points_and_levels([[0, 0]], [3])
+    assert_equal("??", points)
+    assert_equal("B",  levels)
+  end
+
   def test_encode_polyline_1e5__1
     points, levels = @mod.encode_polyline_1e5([[0, 0, 3]])
     assert_equal("??", points)
@@ -34,4 +40,12 @@ class CoreTest < Test::Unit::TestCase
       expected,
       @mod.decode_polyline_1e5("ACACAGAOA_@", "BA@?B"))
   end
+
+=begin
+  def test_encode_and_decode
+    srand(0)
+    points = 20.times.map { [rand * 180 - 90, rand * 360 - 180] }
+    levels = [3] + 18.times.map { rand(4) } + [3]
+  end
+=end
 end
